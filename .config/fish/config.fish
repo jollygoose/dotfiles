6870abc07fb6803604a22d44a6246e71238375e5
jollygoose <85962006+jollygoose@users.noreply.github.com>
2022-03-05 13:33:38 -0500

    added kubectl and docker shortcuts. remove old files.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 export QT_QPA_PLATFORMTHEME=qt5ct
 #export QT_QPA_PLATFORM=wayland
 export PATH="$HOME/.local/bin:$PATH"
-export PATH="$HOME/.bin:$PATH"
+export PATH="$HOME/desktop/code/bin:$PATH"
 export PATH="/usr/local/bin/aws:$PATH"
 export JDK_HOME="/usr/lib/jvm/openjdk-11/"
 export POWERSHELL_TELEMETRY_OPTOUT=1
@@ -11,3 +11,4 @@ export PATH="$HOME/.local/npm/bin:$PATH"
 export CHROME_EXECUTABLE=chromium
 starship init fish | source
 theme.sh vs-code-dark-plus
+kubectl completion fish | source
